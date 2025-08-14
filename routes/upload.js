06b80d22cda1bb373d4daabd86eb4ab4cf3ba042
CristianDVL <cristian9fernandez9@gmail.com>
2025-08-13 20:55:25 -0500

    test 4

--- routes/upload.js
+++ routes/upload.js
@@ -6,7 +6,6 @@ const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: process.env.S3_BUCKET_NAME,
-    acl: 'public-read',
     key: (req, file, cb) => {
       const fileName = `perfil/${Date.now()}-${file.originalname}`;
       cb(null, fileName);
