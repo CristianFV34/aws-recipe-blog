0e1ad959f7d49769375bd6a80d5a3f53460e5963
CristianDVL <cristian9fernandez9@gmail.com>
2025-08-15 17:48:04 -0500

    Version v3 de aws

--- routes/registro.js
+++ routes/registro.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const { GetCommand, UpdateCommand } = require('@aws-sdk/lib-dynamodb');
+const { PutCommand } = require('@aws-sdk/lib-dynamodb');
 const { dynamodb } = require('./aws-config');
 const upload = require('./upload');
 
@@ -13,24 +13,24 @@ module.exports = (app) => {
         const params = {
             TableName: 'Usuarios',
             Item: {
-            username,        
-            email,               
-            passwordHash,
-            foto: 'images/user-3296.png'
+                username,
+                email,
+                passwordHash,
+                foto: 'images/user-3296.png'
             },
             ConditionExpression: 'attribute_not_exists(username)'
         };
 
         try {
-            await dynamodb.put(params).promise();
+            await dynamodb.send(new PutCommand(params));
             res.send('Usuario registrado con éxito');
         } catch (err) {
-            if (err.code === 'ConditionalCheckFailedException') {
-            res.status(400).send('El nombre de usuario ya existe');
+            if (err.name === 'ConditionalCheckFailedException') {
+                res.status(400).send('El nombre de usuario ya existe');
             } else {
-            console.error(err);
-            res.status(500).send('Error al registrar usuario');
+                console.error(err);
+                res.status(500).send('Error al registrar usuario');
             }
         }
     });
-}
+};
